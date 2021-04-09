beb45b5332b66c5c410714100382db1060665f22
Diogo Emannuel Ferreira Rodrigues <diogoemannuel.rodrigues1@gmail.com>
2021-04-09 18:18:01 -0300

    Adding the class model

---
+++ src/main/java/com/example/demo/model/Pessoa.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Pix pix;
+
+	public Pessoa() {
+	}
+	
+	
+	
+
+}
